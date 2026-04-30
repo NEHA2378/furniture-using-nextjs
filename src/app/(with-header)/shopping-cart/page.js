66a991aceed2887cd23c48dd9ee74244f206c3bb
NEHA2378 <121502417+NEHA2378@users.noreply.github.com>
2026-04-30 18:53:40 +0530

    product detail page added

--- src/app/(with-header)/shopping-cart/page.js
+++ src/app/(with-header)/shopping-cart/page.js
@@ -116,7 +116,7 @@ export default function ShoppingCart() {
                                             />
                                         </td>
                                         <td className="p-3 text-center border">
-                                            <Link href={`/my-products/${item.id}`}>
+                                            <Link href={`/my-products/${item._id}`}>
                                                 <img
                                                     src={item.image}
                                                     className="w-[120px] mx-auto object-contain"
